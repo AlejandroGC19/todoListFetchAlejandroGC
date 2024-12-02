5b8977afdac4410d8fd6e20e6952b8da04c057af
Alejandro Garrocho Cruz <101363464+AlejandroGC19@users.noreply.github.com>
2024-12-02 10:43:25 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,35 +1,210 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import List from "./list";
 
 //create your first component
 const Home = () => {
 	const [textInput, setTextInput] = useState("");
-	const [list, setList] = useState([]);
+	const [userInput, setUserInput] = useState("");
+	const [allTasks, setAllTasks] = useState(false);
+
+	const [listServer, setListServer] = useState([]);
+	const [userName, setUserName] = useState("");
+
+	const createUser = async (nameText) => {
+		try{
+			let response = await fetch("https://playground.4geeks.com/todo/users/"+nameText,{
+				method:'POST'
+			});
+			setUserName(nameText);
+			getTodoList(nameText);
+			if (response.ok){
+				console.log("User created:");
+				return true;
+			} else{
+				if (response.status === 400) {
+					console.log("User loaded");
+				} else{
+					console.log("User can't be created");
+				}
+				return false;
+			}
+		} catch (error){
+			console.log("FETCH ",error);
+			return ;
+		}
+	}
+	const deleteUser = async () => {
+		try{
+			let response = await fetch("https://playground.4geeks.com/todo/users/"+userName,{
+				method:'DELETE'
+			});
+			if (response.ok){
+				console.log("User deleted: " + userName);
+				return true;
+			} else{
+				console.log("User can't be deleted");
+				return false;
+			}
+		} catch (error){
+			console.log("FETCH ",error);
+			return ;
+		}
+	}
+	
+	const createToDo = async (taskText) => {
+		try{
+			let response = await fetch("https://playground.4geeks.com/todo/todos/"+userName,{
+				method:'POST',
+				body: JSON.stringify({
+					"label": taskText,
+					"is_done": false
+				  }),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			if (response.ok){
+				let data = await response.json();
+				console.log("Task added");
+				console.log(data);
+				setListServer([...listServer, data]);
+				//localStorage.setItem(data.id,data);
+				
+				return true;
+			} else{
+				console.log("Task no added");
+				return false;
+			}
+		} catch (error){
+			console.log("FETCH ",error);
+			return ;
+		}
+	}
+	
+	const updateTodoList = async (taskID,taskText) => {
+		try{
+			let response = await fetch("https://playground.4geeks.com/todo/todos/"+ taskID,{
+				method:'PUT',
+				body: JSON.stringify({
+					"label": taskText,
+					//"is_done": !visible
+					"is_done": true
+				}),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+			if (response.ok){
+				console.log("Task completed: " + taskText);
+				getTodoList(userName);
+				return true;
+			}
+		} catch (error){
+			console.log("FETCH ",error);
+			return ;
+		}
+	}
+	
+	const getTodoList = async (nameText) => {
+		try{
+			let response = await fetch("https://playground.4geeks.com/todo/users/"+nameText,{
+				method:'GET'
+			});
+			if (response.ok){
+				let data = await response.json();
+				console.log("Tasks on server:");
+				console.log(data.todos);
+				setListServer(data.todos);
+				return true;
+			}
+		} catch (error){
+			console.log("FETCH ",error);
+			return ;
+		}
+	}
+
+	const deleteList = async (taskID,taskText) => {
+		try{
+			let response = await fetch("https://playground.4geeks.com/todo/todos/"+taskID,{
+				method:'DELETE'
+			});
+			if (response.ok){
+				console.log("Task deleted: " + taskText);
+				return true;
+			}else{
+				console.log("Task no deleted: " + taskText);
+				return false;
+			}
+		} catch (error){
+			console.log("FETCH ",error);
+			return ;
+		}
+	}
 
 	function addTask (e){
 		if (e.key === "Enter") {
 			if (e.target.value === "") alert("Task can't be empty");
+			else if (userName === "") alert("Introduce your user");
 			else {
-				localStorage.setItem(textInput,textInput)
-				setList([...list, textInput]);
+				createToDo(textInput);
 				setTextInput("");
 			}
 		}
 	}
+	
+	function addUser (e){
+		if (e.key === "Enter") {
+			if (e.target.value === "") alert("User can't be empty");
+			else {
+				createUser(userInput);
+				setUserInput("");
+			}
+		}
+	}
+	
+	function updateTask(index){
+		
+		updateTodoList(index.target.id,index.target.ariaReadOnly);
+		//updateTodoList(index.target.id,index.target.ariaReadOnly,index.target.ariaHidden);
+		setListServer(listServer);
+	}
+	
+	function deleteAllTask () {
+		listServer.map((task)=>{
+			deleteList(task.id, task.label);
+		});
+		setListServer([]);
+	}
 
-	function deleteTask(index){
-		let resultado = [...list];
-		resultado.splice(index.target.id,1);
-		setList(resultado);
+	function deleteUserFunc (){
+		deleteUser();
+		setUserName("")
+		getTodoList();
 	}
 
 	return (
 		<>
-			<h1 className="title mb-5 text-center">todos</h1>
-			<div className="container main d-flex flex-column justify-content-center w-75">
+			<h1 className="title mb-3 text-center">todos</h1>
+			<h2 className="text-center my-3">User: {userName}</h2>
+			<div className="container d-flex justify-content-center gap-3 my-4">
+				<div className="row d-flex justify-content-between justify-items-center w-75">
+					<div className="col-12 col-lg-5 my-4">
+						<div className="input-group flex-nowrap">
+							<span className="input-group-text" id="addon-wrapping"><i className="fa-regular fa-user"></i></span>
+							<input type="text" className="form-control" onChange={e => setUserInput(e.target.value)} onKeyDown={addUser} value={userInput} placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>
+						</div>
+					</div>
+					<button className="btn btn-secondary col-5 col-lg-2 m-2" onClick={()=>setAllTasks(!allTasks)}>{allTasks ? "Hide done" : "Show all"} tasks</button>
+					<button className="btn btn-warning col-5 col-lg-2 m-2" onClick={deleteUserFunc}>Delete user</button>
+					<div className="d-flex justify-content-center col-lg-2">
+						<button className="btn btn-danger col-12 m-2" onClick={deleteAllTask}>Clear TODO list</button>
+					</div>
+				</div>
+			</div>
+			<div className="container main d-flex flex-column justify-content-center w-75 mb-3">
 				<input className="w-100 border-0 mb-3" id="input" type="text" onChange={e => setTextInput(e.target.value)} onKeyDown={addTask} value={textInput} placeholder="What needs to be done?"/>
-				<List list={list} deleteTask={deleteTask}/>
-				<p className="items-left text-start ps-2 mt-3">{list.length} {list.length === 1 ? "item" : "items"} left</p>
+				<List list={listServer} updateTask={updateTask} allTasks={allTasks}/>
+				<p className="items-left text-start ps-2 mt-3">{listServer.filter((task)=>!task.is_done).length} {listServer.filter((task)=>!task.is_done).length === 1 ? "item" : "items"} left</p>
 			</div>
 		</>
 	);
--- src/js/component/list.jsx
+++ src/js/component/list.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 
-const List = ({list, deleteTask}) => {
+const List = ({list, updateTask,allTasks}) => {
     return (
         <ul className="list-group pe-3 fs-1">
-            {
-            list.map((task,index)=>{
+            {allTasks ?
+            list.map((task)=>{
                 return (
-                    <li key={index} className="list-group-item border-0 d-flex justify-content-between p-0 w-100">
+                    <li key={task.id} className={task.is_done ? "list-group-item border border-success rounded d-flex justify-content-between p-0 w-100 my-1" : "list-group-item border-0 d-flex justify-content-between p-0 w-100 my-1"}>
+                        <span className="taskText ms-2">
+                            {task.label}
+                        </span>
+                        <span className="d-flex align-items-center">
+                            <button className={(!task.is_done && "deleteButton") + " border-0 me-2"} onClick={updateTask}><i id={task.id} className={task.is_done ? "fa-solid fa-circle-check" : "fa-duotone fa-solid fa-x fa-xs text-danger"} aria-readonly={task.label}></i></button>
+                        </span>
+                    </li>
+                )
+            }) 
+            : list.map((task)=>{
+                return (!task.is_done &&
+                    <li key={task.id} className="list-group-item border-0 d-flex justify-content-between p-0 w-100 my-1">
                         <span className="taskText">
-                            {task}
+                            {task.label}
                         </span>
                         <span>
-                            <button id={index} className="deleteButton border-0 mb-2" onClick={deleteTask}><i className="fa-duotone fa-solid fa-x fa-xs text-danger"></i></button>
+                            <button className="deleteButton border-0 mb-2" onClick={updateTask}><i id={task.id} className="fa-duotone fa-solid fa-x fa-xs text-danger" aria-readonly={task.label}></i></button>
                         </span>
                     </li>
                 )
